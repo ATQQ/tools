a9201e45cc87718393ffb40284a1e03762c6471e
sugar <engineerzjl@foxmail.com>
2022-09-27 23:41:09 +0800

    feat: 完善渐进的单文件检测的demo

---
+++ packages/cli/es-check/__test__/demos/more-error/4.ts
@@ -0,0 +1,48 @@
+import * as acorn from 'acorn'
+import * as acornWalk from 'acorn-walk'
+
+const code = `var { boolean:hello } = { }`
+const ast = acorn.parse(code, {
+  ecmaVersion: 'latest'
+})
+
+const codeErrorList: any[] = []
+acornWalk.full(ast, (node, _state, _type) => {
+  // 节点对应的源码
+  const codeSnippet = code.slice(node.start, node.end)
+  let isValidCode = true
+
+  // 判断代码片段 是否合法
+  try {
+    acorn.parse(codeSnippet, {
+      ecmaVersion: 'latest'
+    })
+  } catch (_) {
+    isValidCode = false
+  }
+
+  // 不合法不处理
+  if (!isValidCode) {
+    return
+  }
+
+  try {
+    acorn.parse(codeSnippet, {
+      ecmaVersion: '5'
+    } as any)
+  } catch (error: any) {
+    // 在这里输出错误片段和解析报错原因
+    const isRepeat = codeErrorList.find((e) => {
+      return e.start >= node.start && e.end <= node.end
+    })
+    if (!isRepeat) {
+      codeErrorList.push({
+        codeSnippet,
+        message: error.message,
+        start: node.start,
+        end: node.end
+      })
+    }
+  }
+})
+console.log(codeErrorList)
