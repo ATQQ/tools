53554acddfc1ec3c403c9cb33fe3784508a6ce24
sugar <engineerzjl@foxmail.com>
2023-02-11 23:08:44 +0800

    feat: 新增博客内容格式一键转换插件

---
+++ packages/cli/dynamic-cli/plugins/cli-plugin-blog/tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  splitting: false,
+  target:'es2015',
+  sourcemap: true,
+  clean: true,
+  dts: true,
+  entryPoints: ['src/index.ts'],
+  external:['@sugarat/cli']
+})
