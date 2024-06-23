ea6ff0414b8335b27009ba658e9631f34fc4fed0
sugar <engineerzjl@foxmail.com>
2024-06-23 11:19:23 +0800

    feat: 支持CLI查看和修改服务端配置

--- packages/cli/dynamic-cli/plugins/cli-plugin-ep/src/action.ts
+++ packages/cli/dynamic-cli/plugins/cli-plugin-ep/src/action.ts
@@ -1,49 +1,22 @@
 import { readJSONFIle, getCLIConfig } from '@sugarat/cli'
-import { exec, execSync } from 'child_process'
+import { exec } from 'child_process'
 import path from 'path'
-import qiniu from 'qiniu'
-import fs from 'fs'
-import axios from 'axios'
-import { intro, outro, spinner, cancel } from '@clack/prompts'
+import {
+  intro,
+  outro,
+  spinner,
+  cancel,
+  select,
+  isCancel,
+  text,
+  confirm
+} from '@clack/prompts'
 import chalk from 'chalk'
 import { promisify } from 'util'
 import semver from 'semver'
+import fs from 'fs'
 
 const execAsync = promisify(exec)
-export function isCmdExist(
-  cmd: string,
-  ops?: {
-    name?: string
-    installCommand?: string
-    tip?: string
-  }
-) {
-  const { name, installCommand, tip } = ops || {}
-  const softwareName = name || cmd
-  return new Promise((resolve, reject) => {
-    exec(`command -v ${cmd}`, (err, stdout, stderr) => {
-      if (err) {
-        console.log(`❌ ${softwareName}`)
-        if (!installCommand) {
-          console.log(`💡 ${tip || `请手动安装 ${softwareName}`}`)
-          reject()
-          return
-        }
-        console.log(`🔧 准备安装 ${softwareName}`)
-
-        execSync(installCommand, {
-          stdio: 'inherit',
-          cwd: process.cwd()
-        })
-        console.log(`✅ 安装完成 ${softwareName}`)
-        resolve(true)
-        return
-      }
-      console.log(`✅ ${softwareName}`)
-      resolve(true)
-    })
-  })
-}
 
 function checkNodeVersion() {
   const nodeVersionSpinner = spinner()
@@ -84,6 +57,7 @@ async function checkPNPM() {
   }
   pnpmSpinner.stop('✅ pnpm已安装')
 }
+
 export async function checkMachineEnv() {
   console.log()
   intro(chalk.inverse(' 检查环境配置情况 '))
@@ -96,308 +70,125 @@ export async function checkMachineEnv() {
 
   outro(`完成检查`)
 }
-export const CompressPkgName = (type: string, version: string) => {
-  return `EasyPicker_${type}_${version}.tar.gz`
-}
-export function initMysql(dbName: string, user: string, password: string) {
-  execSync(
-    `curl https://script.sugarat.top/shell/ep/init-db.sh | bash -s ${dbName} ${user} ${password}`,
-    {
-      cwd: process.cwd(),
-      stdio: 'inherit'
-    }
-  )
-}
 
-export function packDist(type: string) {
-  const pkgJSON = readJSONFIle(path.resolve(process.cwd(), 'package.json'))
-  const compressPkgName = CompressPkgName(type, pkgJSON.version)
-
-  if (fs.existsSync(path.join(process.cwd(), compressPkgName))) {
-    fs.rmSync(path.join(process.cwd(), compressPkgName))
-  }
-
-  execSync(`tar -zvcf ${compressPkgName} package.json .env dist`, {
-    stdio: 'ignore',
-    cwd: process.cwd()
-  })
-  console.log(`✅ 产物归档完成`)
-  execSync(`du -h ${compressPkgName}`, {
-    stdio: 'inherit',
-    cwd: process.cwd()
-  })
-  return compressPkgName
-}
-
-export function uploadPkg(filename: string) {
-  const pkgJSON = readJSONFIle(path.resolve(process.cwd(), 'package.json'))
-
-  return new Promise((resolve, reject) => {
-    console.log('🔧 正在上传文件', filename)
-    const qiniuConfig = getCLIConfig('qiniu')
-    const {
-      bucket,
-      accessKey,
-      secretKey,
-      base = `dist/easypicker/`
-    } = qiniuConfig || {}
-    const mac = new qiniu.auth.digest.Mac(accessKey, secretKey)
-    const putPolicy = new qiniu.rs.PutPolicy({
-      scope: bucket
-    })
-    const uploadToken = putPolicy.uploadToken(mac)
-
-    const config = new qiniu.conf.Config({
-      // 空间对应的机房
-      zone: qiniu.zone.Zone_z0,
-      useCdnDomain: true
-    })
-    const localFile = path.resolve(process.cwd(), filename)
-
-    const formUploader = new qiniu.form_up.FormUploader(config)
-    const putExtra = new qiniu.form_up.PutExtra()
-    const key = `${base}${pkgJSON?.version || 'unknown'}/${filename}`
-    // 七牛云文件上传
-    formUploader.putFile(
-      uploadToken,
-      key,
-      localFile,
-      putExtra,
-      (respErr, respBody, respInfo) => {
-        if (respErr) {
-          console.log('❌ 文件上传出错', filename)
-          reject(respErr)
-          throw respErr
-        }
-        if (respInfo.statusCode === 200) {
-          // console.log(respBody)
-          resolve(respInfo)
-          console.log('✅ 资源上传成功', key)
-        } else {
-          console.log('❌ 文件上传失败', filename)
-          reject(respInfo)
-          // console.log(respInfo.statusCode)
-          // console.log(respBody)
-        }
-      }
-    )
-  })
-}
-
-export async function pullPkg(type: string, version: string) {
-  console.log('🔧 准备拉取资源包')
-  const { versionMapUrl, cdn } = getCLIConfig('qiniu.source')
-
-  // 取远程配置
-  const versionMap = (await axios.get(versionMapUrl)).data
-
-  // 取版本号
-  const targetVersion = versionMap[version] || version
-  const pkgName = CompressPkgName(type, targetVersion)
-  // 生成key路径
-  const sourceUrl = `${cdn}/${getCLIConfig(
-    'qiniu.base'
-  )}${targetVersion}/${pkgName}`
-
-  try {
-    const result = await axios.get(sourceUrl, {
-      headers: {
-        'Content-Type': 'multipart/form-data'
-      },
-      responseType: 'arraybuffer'
-    })
-    // 保存资源到本地
-    fs.writeFileSync(pkgName, result.data, 'binary')
-    console.log('✅ 资源包已拉取到本地', pkgName)
-  } catch (error: any) {
-    console.log(error?.message)
-    console.log('❌ 资源不存在')
-    process.exit(0)
-  }
-  return pkgName
-}
-
-export async function getCompressName(
-  type: string,
-  version: string,
-  existName?: string
-) {
-  if (existName && fs.existsSync(path.resolve(process.cwd(), existName))) {
-    return existName
-  }
-  const { versionMapUrl } = getCLIConfig('qiniu.source')
-  // 取远程配置
-  const versionMap = (await axios.get(versionMapUrl)).data
-
-  // 取版本号
-  const targetVersion = versionMap[version] || version
-  const pkgName = CompressPkgName(type, targetVersion)
-  return pkgName
-}
-
-export async function unPkg(type: string, version: string, existName?: string) {
-  const pkgName = await getCompressName(type, version, existName)
-
-  const targetDir = path.resolve(
-    process.cwd(),
-    type === 'client' ? './' : 'easypicker2-server'
-  )
-
-  if (!fs.existsSync(targetDir)) {
-    fs.mkdirSync(targetDir, { recursive: true })
-  }
-
-  execSync(`tar -xf ${pkgName} -C ${targetDir}`, {
-    stdio: 'ignore',
-    cwd: process.cwd()
-  })
-  console.log('✅ 资源包已解压', pkgName)
-}
-export function validServerFile() {
-  console.log('🔍 正在检查相关文件是否完整')
-  const targetDir = path.resolve(process.cwd(), 'easypicker2-server')
-  if (
-    !fs.existsSync(path.join(targetDir, 'dist/index.js')) ||
-    !fs.existsSync(path.join(targetDir, 'package.json'))
-  ) {
-    console.log('❌ 服务端文件不存在', targetDir)
-    process.exit(0)
-  }
-
-  // console.log('🔧 正在进行pnpm依赖安装，请稍等')
-
-  // 切换到淘宝镜像源
-  execSync(`npm config set registry https://registry.npmmirror.com/`, {
-    stdio: 'ignore',
-    cwd: targetDir
-  })
-
-  // 安装依赖
-  execSync(`pnpm install`, {
-    stdio: 'ignore',
-    cwd: targetDir
-  })
-
-  return targetDir
-}
-
-export function deleteService(serverName: string) {
-  // 删除旧的
-  try {
-    execSync(`pm2 delete ${serverName}`, {
-      stdio: 'ignore',
-      cwd: process.cwd()
-    })
-  } catch (error) {
-    // TODO：更友好的处理
-    console.log()
-  }
-}
-
-export async function runService(serverName: string) {
-  // 启动
-  try {
-    execSync(
-      `cd easypicker2-server && pm2 start npm --name ${serverName} -- run start`,
-      {
-        stdio: 'ignore',
-        cwd: process.cwd()
-      }
-    )
-    // eslint-disable-next-line no-promise-executor-return
-    await new Promise((res) => setTimeout(res, 2000))
-    execSync(
-      `tail -n8 ${path.join(
-        process.env.HOME || process.env.USERPROFILE || process.cwd(),
-        '.pm2/logs',
-        `${serverName}-out.log`
-      )}`,
-      {
-        stdio: 'inherit',
-        cwd: process.cwd()
-      }
-    )
-    console.log('✅ 服务启动成功', serverName)
-    console.log('可以使用 q ep server --status 查看服务状态')
-    console.log('可以使用 q ep server --log 查看服务日志')
-  } catch (error) {
-    console.log('❌ 服务启动失败')
-  }
-}
-
-export function restartService(serverName: string) {
-  try {
-    execSync(`pm2 restart ${serverName}`, {
-      stdio: 'ignore',
-      cwd: process.cwd()
-    })
-  } catch (error) {
-    console.log('❌ 服务重启失败')
-    return
-  }
-  console.log('✅ 服务重启成功')
-}
-
-export function stopService(serverName: string) {
-  try {
-    execSync(`pm2 stop ${serverName}`, {
-      stdio: 'ignore',
-      cwd: process.cwd()
-    })
-  } catch (error) {
-    console.log('❌ 停止服务失败')
-    return
-  }
-  console.log(`✅ 已停止服务${serverName}`)
-}
-export function checkServiceStatus(serverName: string) {
-  execSync(`pm2 monit ${serverName}`, {
-    stdio: 'inherit',
-    cwd: process.cwd()
-  })
-}
-export function checkServiceLog(serverName: string) {
-  execSync(`pm2 logs ${serverName} --out`, {
-    stdio: 'inherit',
-    cwd: process.cwd()
-  })
-}
-export function deployServer(serverName: string) {
-  deleteService(serverName)
-  runService(serverName)
-}
-export function checkServiceList() {
+async function checkServiceList() {
   const serviceList = getCLIConfig('server.list')
-  console.log(serviceList)
-}
-export async function deployPkg(type: string, version: string) {
-  const pkgName = await pullPkg(type, version)
-  await unPkg(type, version, pkgName)
+  console.log(`========= ${chalk.green('CLI记录列表')} =========`)
+  console.table(serviceList)
+  console.log(`========= ${chalk.green('pm2 list')} =========`)
+  const { stdout } = await execAsync('pm2 list', {
+    cwd: process.cwd()
+  })
+  console.log(stdout)
 }
 
-export async function checkConfig(type: string) {
-  const cyanColor = (str: string) => `[36m${str}[39m`
-  try {
-    const userData = readJSONFIle(
-      path.join(process.cwd(), 'easypicker2-server', 'user-config.json')
-    )
-    if (!type) {
-      console.log(
-        `你应该在执行脚本后添加 ${cyanColor('<type>')} 参数，支持如下值`
-      )
-      console.log([...new Set(userData.map((v: any) => v.type))])
-      console.log('例如', cyanColor('q ep --config user'))
-      process.exit(1)
-    }
-    const getTypeObj = (_type: string) =>
-      userData
-        .filter((v: any) => v.type === _type)
-        .reduce((pre: any, cur: any) => {
-          pre[cur.key] = cur.value
-          return pre
-        }, {})
-    console.table(getTypeObj(type))
-  } catch (error) {
-    console.log('❌', '目标目录下不存在 user-config.json 文件')
+async function checkConfig() {
+  const userConfigPath = path.resolve(
+    process.cwd(),
+    'easypicker2-server/user-config.json'
+  )
+  if (!fs.existsSync(userConfigPath)) {
+    cancel(`不存在 user-config.json 文件 ${userConfigPath}`)
+    return process.exit(0)
   }
+  const userConfig = readJSONFIle(userConfigPath)
+  const types = [
+    ...new Set(userConfig.map((item: any) => item.type))
+  ] as string[]
+
+  const type = await select({
+    message: '请选择配置类型',
+    options: types.map((item) => ({
+      value: item,
+      label: item
+    }))
+  })
+
+  if (isCancel(type)) {
+    cancel('取消')
+    return process.exit(0)
+  }
+  const filterConfig = userConfig.filter((item: any) => item.type === type)
+  console.log(`========= ${chalk.green(type)} 配置详情 =========`)
+  console.table(filterConfig)
+}
+
+async function rewriteConfig() {
+  const userConfigPath = path.resolve(
+    process.cwd(),
+    'easypicker2-server/user-config.json'
+  )
+
+  if (!fs.existsSync(userConfigPath)) {
+    cancel(`不存在 user-config.json 文件 ${userConfigPath}`)
+    return process.exit(0)
+  }
+  const userConfig = readJSONFIle(userConfigPath)
+  const types = [
+    ...new Set(userConfig.map((item: any) => item.type))
+  ] as string[]
+
+  const type = await select({
+    message: '请选择要修改的配置类型',
+    options: types.map((item) => ({
+      value: item,
+      label: item
+    }))
+  })
+  if (isCancel(type)) {
+    cancel('取消')
+    return process.exit(0)
+  }
+  const filterConfig = userConfig.filter((item: any) => item.type === type)
+  for (const cfg of filterConfig) {
+    const newValue = await text({
+      message: `重新设置${cfg.type}.${cfg.key}值`,
+      initialValue: cfg.value
+    })
+    if (isCancel(newValue)) {
+      cancel('取消')
+      return process.exit(0)
+    }
+    cfg.value = newValue
+  }
+  const confirmRewrite = await confirm({
+    message: `确认修改${type}配置？`,
+    initialValue: true
+  })
+  if (isCancel(confirmRewrite) || !confirmRewrite) {
+    cancel('取消')
+    return process.exit(0)
+  }
+
+  fs.writeFileSync(userConfigPath, JSON.stringify(userConfig, null, 2))
+}
+export async function checkService() {
+  console.log()
+  intro(chalk.inverse(' 检查服务相关配置 '))
+
+  const operator = await select({
+    message: '请选择操作',
+    options: [
+      { value: 'config', label: '配置详情' },
+      { value: 'rewrite', label: '修改配置' },
+      { value: 'list', label: '服务列表' }
+    ]
+  })
+  if (isCancel(operator)) {
+    cancel('取消')
+    return process.exit(0)
+  }
+
+  if (operator === 'list') {
+    await checkServiceList()
+  }
+  if (operator === 'config') {
+    await checkConfig()
+  }
+  if (operator === 'rewrite') {
+    await rewriteConfig()
+    outro(`✅ 配置修改完成，重启后端服务后生效`)
+    return
+  }
+  outro(`bye bye ^_^ ✋🏻`)
 }
--- packages/cli/dynamic-cli/plugins/cli-plugin-ep/src/index.ts
+++ packages/cli/dynamic-cli/plugins/cli-plugin-ep/src/index.ts
@@ -1,5 +1,5 @@
 import { defineCommand, ICommandDescription } from '@sugarat/cli'
-import { checkMachineEnv, checkServiceList } from './action'
+import { checkMachineEnv, checkService } from './action'
 import type { ActionType, Options } from './type'
 import { deployMenu } from './menu/deploy'
 
@@ -12,8 +12,6 @@ export default function definePlugin(): ICommandDescription {
         .description(
           `部署兼启动 EasyPicker 服务 (type in [deploy, check, server])`
         )
-        .option('--config [name]', '查看指定配置')
-        .option('--list', '查看服务列表')
         .action(async (type: ActionType, options: Options) => {
           if (type === 'deploy') {
             await deployMenu()
@@ -24,73 +22,9 @@ export default function definePlugin(): ICommandDescription {
             return
           }
 
-          if (type !== 'server') {
-            return
+          if (type === 'server') {
+            await checkService()
           }
-          if (options.list) {
-            checkServiceList()
-          }
-          // const serverDir = validServerFile()
-          // // serverName 和目录绑定
-          // const serverList = getCLIConfig('server.list') || []
-          // const serverInfoIdx = serverList.findIndex(
-          //   (v: any) => v.dir === serverDir
-          // )
-
-          // const serverInfo = serverList[serverInfoIdx] || {}
-          // if (!serverInfo.name) {
-          //   serverInfo.dir = serverDir
-          //   serverInfo.name = options.name || `ep-server-${Date.now()}`
-          //   serverList.push(serverInfo)
-          //   setCLIConfig('server.list', serverList)
-          // }
-          // if (options.name) {
-          //   serverInfo.name = options.name
-          // }
-
-          // console.log('')
-          // console.log('====操作服务信息====')
-          // console.log('= dir:', serverInfo.dir)
-          // console.log('= name:', serverInfo.name)
-          // console.log('====================')
-          // console.log('')
-
-          // // 获取服务名
-          // const serverName = serverInfo.name
-          // if (options.config) {
-          //   checkConfig(options.config)
-          //   return
-          // }
-
-          // if (options.deploy) {
-          //   // 校验目标文件和目录是否存在
-          //   // 部署服务
-          //   deployServer(serverName)
-          //   return
-          // }
-          // if (options.restart) {
-          //   restartService(serverName)
-          //   return
-          // }
-          // if (options.stop) {
-          //   stopService(serverName)
-          //   return
-          // }
-          // if (options.del) {
-          //   delCLIConfig(`server.list.${serverInfoIdx}`)
-          //   deleteService(serverName)
-          //   console.log('✅ 删除服务', serverName)
-          //   return
-          // }
-          // if (options.status) {
-          //   checkServiceStatus(serverName)
-          //   return
-          // }
-          // if (options.log) {
-          //   checkServiceLog(serverName)
-          //   return
-          // }
-          // console.log('🎉 hello easypicker2 🎉')
         })
     }
   })
